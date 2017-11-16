1401c64c35d26a01f80b224cb4bc305a001818fd
Chisomo Lungu <chisomolungu@imac.home>
2017-11-16 19:54:45 +0000

    first commit

---
+++ hello-world.py
@@ -0,0 +1,9 @@
+from sklearn import tree
+# replacing "smooth" with 1 and "bumpy" with 0
+# replacing "orange" with 1 and "apple" with 0
+features = [[140, 1], [130, 1], [150, 0], [170, 0]]
+labels = [0, 0, 1, 1]
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features, labels)
+example = clf.predict([[160, 0]])
+print(example)
