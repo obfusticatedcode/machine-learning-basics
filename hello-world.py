b11fd86b01374a11c9b738549a32707fcc4d1518
Chisomo Lungu <chisomolungu@imac.home>
2017-11-16 20:14:14 +0000

    refactored

--- hello-world.py
+++ hello-world.py
@@ -5,5 +5,4 @@ features = [[140, 1], [130, 1], [150, 0], [170, 0]]
 labels = [0, 0, 1, 1]
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(features, labels)
-example = clf.predict([[160, 0]])
-print(example)
+print(clf.predict([[160, 0]]))
